2325252d2f88026eb0a37e70549223473989deb0
Talha Donmez <talhadonmez@Talhas-MacBook-Pro-2.local>
2020-08-06 18:51:27 -0400

    TestNG

---
+++ src/main/java/com/hubspot/util/ElementUtilPF.java
@@ -0,0 +1,20 @@
+package com.hubspot.util;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class ElementUtilPF {
+	
+	WebDriver driver;
+	
+	public ElementUtilPF (WebDriver driver){
+		this.driver = driver;
+	}
+	
+	public void waitForElementPresent(WebElement element){
+		WebDriverWait wait = new WebDriverWait(driver, 20);
+		wait.until(ExpectedConditions.visibilityOf(element));
+	}
+}
